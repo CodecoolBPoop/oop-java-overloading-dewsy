d183e3976f04e852bc79aeb74d4d3058cfafd2e0
dewsy <dewsyy@gmail.com>
2018-09-09 13:03:00 +0200

    getters/setters done

--- src/com/codecool/uml/overloading/overloading.java
+++ src/com/codecool/uml/overloading/overloading.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class overloading {
 
+    public static int productID = 0;
+    public static int categoryID = 0;
+    public static int supplierID = 0;
+
+
 
     public class Product {
 
@@ -15,7 +20,52 @@ public class overloading {
         private Currency defaultCurrency;
         private ProductCategory productCategory;
         private Supplier supplier;
-        private List<Product> productList
+        private List<Product> productList;
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+        public float getDefaultPrice() {
+            return defaultPrice;
+        }
+
+        public void setDefaultPrice(float defaultPrice) {
+            this.defaultPrice = defaultPrice;
+        }
+
+        public Currency getDefaultCurrency() {
+            return defaultCurrency;
+        }
+
+        public void setDefaultCurrency(Currency defaultCurrency) {
+            this.defaultCurrency = defaultCurrency;
+        }
+
+        public ProductCategory getProductCategory() {
+            return productCategory;
+        }
+
+        public void setProductCategory(ProductCategory productCategory) {
+            this.productCategory = productCategory;
+        }
+
+        public Supplier getSupplier() {
+            return supplier;
+        }
+
+        public void setSupplier(Supplier supplier) {
+            this.supplier = supplier;
+        }
 
     }
 
@@ -25,6 +75,35 @@ public class overloading {
         private String name;
         private String department;
         private String description;
+
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDepartment() {
+            return department;
+        }
+
+        public void setDepartment(String department) {
+            this.department = department;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
     }
     public class Supplier {
 
@@ -33,9 +112,28 @@ public class overloading {
         private String name;
         private String description;
 
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
     }
 
-    public class FeaturedProductCategory {
+    public class FeaturedProductCategory extends ProductCategory {
 
         private Date startDate;
         private Date endDate;
